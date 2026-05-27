d5d6e79dc439aa47c2abc4a6b56141ae5fec35bf
Gautham495 <kinggautham495@gmail.com>
2026-05-27 20:45:56 +0530

    chore: android fix

--- android/src/main/cpp/cpp-adapter.cpp
+++ android/src/main/cpp/cpp-adapter.cpp
@@ -1,8 +1,7 @@
 #include <jni.h>
-#include "NitroSensorsSkitOnLoad.hpp"
+#include "nitrosensorskitOnLoad.hpp"
 
 #include <fbjni/fbjni.h>
-
 
 JNIEXPORT jint JNICALL JNI_OnLoad(JavaVM* vm, void*) {
   return facebook::jni::initialize(vm, []() {
